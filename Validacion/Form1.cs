321a20e4d3f67428479ce7168fd7a8c63242f3c9
AugustoQuispe1 <augustoq93@gmail.com>
2023-08-26 18:32:35 -0300

    lol

--- Validacion/Form1.cs
+++ Validacion/Form1.cs
@@ -20,17 +20,23 @@ namespace Validacion
 
         private void BtnPat_Click(object sender, EventArgs e)
             {
-            string input = txtBoxPat.Text.ToUpper(); 
+            string patron = @"^[A-Z]{3}\d{3}$";
+            string patron2 = @"^[A-Z]{2}\d{3}[A-Z]{2}$";
+            string input = txtBoxPat.Text;
 
-            if (ValidarPatente(input))
+            if (Regex.IsMatch(input, patron))
                 {
-                MessageBox.Show("Patente válida:"+txtBoxPat );
+                MessageBox.Show("Patente valida");
+                }
+            else if (Regex.IsMatch(input, patron2))
+                {
+                MessageBox.Show("Patente valida");
                 }
             else
                 {
-                MessageBox.Show("Patente inválida");
+                MessageBox.Show("Patente invalida");
                 }
-           // ValidarPatVacio();
+            ValidarPatVacio();
 
             }
 
@@ -38,18 +44,21 @@ namespace Validacion
             {
 
             string cuil = txtBoxCuil.Text.Trim();
+          
 
             if (cuil.Length == 11 && cuil.All(char.IsDigit))
                 {
+                string dni = cuil.Substring(2, 8); 
                 string cuilFormateado = $"{cuil.Substring(0, 2)}-{cuil.Substring(2, 8)}-{cuil.Substring(10)}"; 
                 string mensaje = "El CUIL ingresado es: " + cuilFormateado;
+                string mensaje2 = "El DNI es: " + dni;
                 MessageBox.Show(mensaje, "CUIL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show(mensaje2, "DNI", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             else
                 {
                 MessageBox.Show("Ingrese un CUIL válido de 11 números.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-
 
             ValidarCuilVacio();
            
@@ -68,26 +77,28 @@ namespace Validacion
             return ok;
             }
 
-       //// private bool ValidarPatVacio()
-       //     {
-       //     bool ok = true;
+         private bool ValidarPatVacio()
+             {
+             bool ok = true;
 
-       //     if (txtBoxPat.Text == "")
-       //         {
-       //         ok = false;
-       //         MessageBox.Show("Llene el campo vacio");
-       //         }
-       //     return ok;
-       //     }
+             if (txtBoxPat.Text == "")
+                 {
+                 ok = false;
+                 MessageBox.Show("Llene el campo vacio");
+                 }
+             return ok;
+             }
 
 
 
-        private bool ValidarPatente(string patente)
-            {
-            // Expresión regular para validar el formato "LLLNNN"
-            string patron = @"^[A-Z]{3}\d{3}$";
-            return Regex.IsMatch(patente, patron);
-            }
+//private bool ValidarPatente(string patente)
+//            {
+//            // Expresión regular para validar el formato "LLLNNN"
+//            //string patron = @"^[A-Z]{3}\d{3}$";
+//            //string patron2 = @"^[A-Z]{2}\d{3}[A-Z}{2}$";
+//            //return Regex.IsMatch(patente, patron);
+//            //return Regex.IsMatch(patron2, patron);
+//            }
 
         private void txtBoxCuil_TextChanged(object sender, EventArgs e)
             {
